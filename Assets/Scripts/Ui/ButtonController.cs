acecce3d783ce7ee3d1e40e45b7482c7f24898ff
Unknown <keslerth.c@gmail.com>
2019-09-28 20:24:28 -0600

    Menu principal

---
+++ Assets/Scripts/Ui/ButtonController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public List<GameObject> buttons;
+    public int actualIndex = 0;
+    private int maxIndex;
+    private bool keyDown = false;
+    // Start is called before the first frame update
+    void Start() {
+        maxIndex = buttons.Count - 1;    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Vertical") != 0){
+            if(!keyDown){
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    if (actualIndex < maxIndex)
+                    {
+                        actualIndex++;
+                    }else{
+                        actualIndex = 0;
+                    }
+                }
+                else if (Input.GetAxis("Vertical") > 0)
+                {
+                    if (actualIndex > 0)
+                    {
+                        actualIndex--;
+                    }else{
+                        actualIndex = maxIndex;
+                    }
+                }
+                keyDown = true;
+            }
+        }
+        else{
+            keyDown = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/button.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    public int thisIndex;
+    private Animator anim;
+    [SerializeField] ButtonController buttonController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(buttonController.actualIndex == thisIndex){
+            anim.SetBool("hover", true);
+            if(Input.GetAxis("Submit") == 1){
+                anim.SetBool("press", true);
+            }
+            //else if (si la animacion de presionar esta encendida)
+            //ejecutar la accion de abrir ese otro escenario
+        }else{
+            anim.SetBool("hover", false);
+        }
+    }
+}
